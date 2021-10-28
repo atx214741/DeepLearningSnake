0d481fed6134eec6ee0c33211b6294bf4658b143
atx214741 <hoehgidhsghs@gmail.com>
2021-10-28 20:35:05 +0800

    init

---
+++ src/main/java/com/lilium/snake/game/helper/Direction.java
@@ -0,0 +1,20 @@
+package com.lilium.snake.game.helper;
+
+public enum Direction {
+    /**
+     * Player will move up.
+     */
+    UP,
+    /**
+     * Player will move right.
+     */
+    RIGHT,
+    /**
+     * Player will move down.
+     */
+    DOWN,
+    /**
+     * Player will move left.
+     */
+    LEFT
+}
---
+++ src/main/java/com/lilium/snake/network/GameObservationSpace.java
@@ -0,0 +1,45 @@
+package com.lilium.snake.network;
+
+import com.lilium.snake.network.util.NetworkUtil;
+import org.deeplearning4j.rl4j.space.ObservationSpace;
+import org.nd4j.linalg.api.ndarray.INDArray;
+
+/**
+ * Game observation space. Shape is [1, 4] as we observe 4 inputs. Starting from the snake head we "look" at position
+ * that is UP, RIGHT, DOWN and LEFT of the head.
+ *
+ * @author mirza
+ */
+public class GameObservationSpace implements ObservationSpace<GameState> {
+    private static final double[] LOWS = GameObservationSpace.createValueArray(NetworkUtil.LOW_VALUE);
+    private static final double[] HIGHS = GameObservationSpace.createValueArray(NetworkUtil.HIGH_VALUE);
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public int[] getShape() {
+        return new int[0];
+    }
+
+    @Override
+    public INDArray getLow() {
+        return null;
+    }
+
+    @Override
+    public INDArray getHigh() {
+        return null;
+    }
+
+    private static double[] createValueArray(final double value) {
+        final double[] values = new double[NetworkUtil.NUMBER_OF_INPUTS];
+        for (int i = 0; i < NetworkUtil.NUMBER_OF_INPUTS; i ++) {
+            values[i] = value;
+        }
+
+        return values;
+    }
+}
